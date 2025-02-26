7fd5eb8d0c8d24145c943e85b0e6c02ce6350be2
prabhjots188 <psg188200799@gmail.com>
2025-02-26 06:49:58 +0530

    new

---
+++ dwm/dwm_preinstalls.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+package="libx11-dev libxinerama-dev libxft-dev feh j4-dmenu-desktop lxpolkit brightnessctl" 
+
+sudo apt install $package >> installs.log
---
+++ scripts/screenshot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scrot ~/Pictures/Screenshots/%b%d::%H%M%S.png
+echo -en "\007"
